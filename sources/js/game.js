af63b304a51adfdc31d9a0c16b757cff894f1242
Kervin Arias <kervinarias@Kervins-MacBook-Pro.local>
2019-12-08 15:55:57 +0000

    background added to canvas

--- sources/js/game.js
+++ sources/js/game.js
@@ -1,10 +1,14 @@
 // Upload images
+var background = new Image();
 var zombomb = new Image();
+
+background.src = "sources/img/canvas.jpg";
 zombomb.src = "sources/img/zombie.gif";
 
 //Main var index for game
 x = 45;
 y = 120;
+
 
 //Canvas Section
 function draw() {
@@ -12,9 +16,14 @@ function draw() {
     var ctx = canvas.getContext("2d");
     canvas.width = 300;
     canvas.height = 500;
+    ctx.drawImage(background, 0, 0);
     ctx.drawImage(zombomb, x, y, 50, 50);
 
 }
+
+
+
+
 
 
 window.onload = draw;
--- sources/js/zombomb.js
+++ sources/js/zombomb.js
@@ -1,22 +1,22 @@
 // Upload images
 var zombomb = new Image();
 zombomb.src = "sources/img/zombie.gif";
-zombomb.onload = () => { requestAnimationFrame(mainLoop) }
 
 //var for components
-var x = 35;
-var y = 230;
-var angleInDegrees = 0;
 
 
 function startGame() {
     var canvas = document.getElementById('gameArea');
     var ctx = canvas.getContext('2d');
-    ctx.drawImage(zombomb, x, y);
+    gameBomb = new component(zombomb, "image");
 
     ctx.save();
     ctx.rotate(0.17);
     ctx.drawImage(zombomb, x, y);
     ctx.restore();
+
+}
+
+gameBomb {
 
 }
