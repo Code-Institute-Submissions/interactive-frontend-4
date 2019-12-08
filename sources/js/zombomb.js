d3b903af20dd5336485aed1a8a06fc29c2cf4488
Kervin Arias <kervinarias@Kervins-MacBook-Pro.local>
2019-12-08 11:25:44 +0000

    rotation added to ctx zombomb

--- sources/js/zombomb.js
+++ sources/js/zombomb.js
@@ -1,22 +1,22 @@
 // Upload images
 var zombomb = new Image();
 zombomb.src = "sources/img/zombie.gif";
+zombomb.onload = () => { requestAnimationFrame(mainLoop) }
 
 //var for components
-
 var x = 35;
 var y = 230;
-var gravity = 1.5;
+var angleInDegrees = 0;
+
 
 function startGame() {
     var canvas = document.getElementById('gameArea');
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(zombomb, x, y);
 
-        ctx.rotate(150);
-        ctx.drawImage(zombomb, x, y);
-    }
+    ctx.save();
+    ctx.rotate(0.17);
+    ctx.drawImage(zombomb, x, y);
+    ctx.restore();
 
 }
-
-startGame();
