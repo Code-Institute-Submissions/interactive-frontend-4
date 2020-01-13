f4eb0deb7b80811ae224cd3c0e295e84282cafcc
Kervin Arias <kervinarias@kervins-mbp.home>
2020-01-13 19:36:34 +0000

    error: bullets not shooting

--- sources/js/game.js
+++ sources/js/game.js
@@ -3,19 +3,29 @@ $(document).ready(function() {
     let player = '<div id="player"><img src="sources/img/human.gif"></div>';
     let zombomb = '<div id="zombomb"><img src="sources/img/zombie.png"></div>';
     $("#gameArea").append(player, zombomb);
+
+    // Inspire on this answer on stackoverflow https://stackoverflow.com/questions/54680988/jquery-animate-a-div-character-in-a-game
     $("#gameArea").mousemove(function(event) {
         $("#zombomb").css({ left: event.pageX })
         $("#player").css({ left: event.pageX })
     })
 
-    // CODE FOR TESTING ---------
-    //  var movePlayer = $("#player");
-    //  var xp = 0,
-    //      yp = 0;
-    //   var loop = setInterval(function() {
-    // change 12 to alter damping higher is slower
-    //      xp += (mouseX - xp) / 12;
-    //      yp += (mouseY - yp) / 12;
-    //      movePlayer.css({ left: xp, top: yp });
-    //      }, 500);
+    $("#zombomb").click(function() {
+        console.log('is working')
+        $(this).toggle("bounce", { times: 3 }, "slow");
+    });
+
 });
+
+
+
+// CODE FOR TESTING ---------
+//  var movePlayer = $("#player");
+//  var xp = 0,
+//      yp = 0;
+//   var loop = setInterval(function() {
+// change 12 to alter damping higher is slower
+//      xp += (mouseX - xp) / 12;
+//      yp += (mouseY - yp) / 12;
+//      movePlayer.css({ left: xp, top: yp });
+//      }, 500);
