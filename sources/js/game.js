92ab86e5835a7d7240f6f4e7c218b88b5027018c
Kervin Arias <kervinarias@kervins-mbp.home>
2020-01-14 19:28:10 +0000

    failed calling back bullet

--- sources/js/game.js
+++ sources/js/game.js
@@ -14,6 +14,10 @@ $(document).ready(function() {
         $("#zombomb").addClass("shoot", 5000);
     });
 
+    setTimeout(function() {
+        ("#zombomb").removeClass("shoot");
+    }, 300);
+
 });
 
 
