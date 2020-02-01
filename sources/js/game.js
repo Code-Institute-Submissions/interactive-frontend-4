4ea5c9ae0fa5e916e3ac3e85e40cdb10c608a18a
Kervin Arias <kervinarias@Kervins-MacBook-Pro.local>
2020-02-01 11:11:49 +0000

    fix player position on game area

--- sources/js/game.js
+++ sources/js/game.js
@@ -1,3 +1,4 @@
+// Game Area Set
 const canvas = document.getElementById('gameArea');
 
 const app = new PIXI.Application({
@@ -6,10 +7,11 @@ const app = new PIXI.Application({
     height: window.innerHeight,
 });
 
+// Player added to the game Area
 const texture = PIXI.Texture.from('sources/img/player.png');
 const player = new PIXI.Sprite(texture);
 player.x = app.renderer.width / 2;
-player.y = app.renderer.height / 2;
+player.y = app.renderer.height / 1.3;
 player.anchor.x = 0.5;
 player.anchor.y = 0.5;
 app.stage.addChild(player);
